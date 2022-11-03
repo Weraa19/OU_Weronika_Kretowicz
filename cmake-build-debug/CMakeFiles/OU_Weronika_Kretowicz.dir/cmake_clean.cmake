f5405a02083ffc8dcb88056100d8909003b88c47
Weronika Kretowicz <235903@edu.p.lodz.pl>
2022-11-03 13:03:16 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_Weronika_Kretowicz.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_Weronika_Kretowicz.dir/main.c.o"
+  "OU_Weronika_Kretowicz"
+  "OU_Weronika_Kretowicz.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_Weronika_Kretowicz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OU_Weronika_Kretowicz.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OU_Weronika_Kretowicz.
+# This may be replaced when dependencies are built.
